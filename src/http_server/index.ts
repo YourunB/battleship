b6149acdc8aed535ebb790af30be30ffb25b7f5d
yourunb <bxyura@gmail.com>
2024-10-23 19:48:53 +0300

    feat: add index TS for http_server

---
+++ src/http_server/index.ts
@@ -0,0 +1,29 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import * as http from 'http'
+
+export const createServerHTTP = () => {
+
+  const port = parseInt(process.env.HTTP_PORT!, 10) || 8181
+
+  const server = http.createServer((req, res) => {
+      const __dirname = path.resolve(path.dirname(''))
+      const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url)
+      fs.readFile(file_path, (err, data) => {
+        if (err) {
+          res.writeHead(404)
+          res.end(JSON.stringify(err))
+          return
+        }
+        res.writeHead(200)
+        res.end(data)
+      })
+  })
+
+  server.listen(
+      port,
+      () => { console.log(`${('[HTTP SERVER]')} Started on port ${(port)}.`) }
+  )
+
+  return server
+}
