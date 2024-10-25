6085bb206bfb538d6c3f0276b31ab72299036410
yourunb <bxyura@gmail.com>
2024-10-25 11:17:27 +0300

    feat: add user to room controller

---
+++ src/ws_server/controllers/addUserToRoom.ts
@@ -0,0 +1,28 @@
+import { roomsDB } from "../data/roomsDB"
+import { connectionsDB } from "../data/connectionsDB"
+import { gamesDB } from "../data/gamesDB"
+
+export const addUserToRoom = (data: any, connectionId: any) => {
+  const { indexRoom } = data as unknown as any
+
+  const player = connectionsDB.getConnection(connectionId)?.player
+  if (!player) { return }
+
+  const room = roomsDB.addRoomUser(indexRoom, player)
+  if (!room) { return }
+  if (room.roomUsers.length < 2) { return }
+
+  const game = gamesDB.createGame(room.roomId)
+
+  gamesDB.setGamePlayers(game.gameId, room.roomUsers)
+  roomsDB.closeRoomsWithPlayer(player)
+
+  connectionsDB.sendData(
+    roomsDB.getUpdateRoomData()
+  )
+
+  gamesDB.getGameData(game.gameId)
+    .forEach(({ type, data, connectionId }: any) => {
+      connectionsDB.sendData({ type, data }, connectionId)
+    })
+}
