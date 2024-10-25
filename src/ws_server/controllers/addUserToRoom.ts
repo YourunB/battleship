9f5980c9c41de4f8c766fd738d42cba9dec04ba8
yourunb <bxyura@gmail.com>
2024-10-25 11:18:17 +0300

    refactor: formate code

--- src/ws_server/controllers/addUserToRoom.ts
+++ src/ws_server/controllers/addUserToRoom.ts
@@ -4,22 +4,18 @@ import { gamesDB } from "../data/gamesDB"
 
 export const addUserToRoom = (data: any, connectionId: any) => {
   const { indexRoom } = data as unknown as any
-
   const player = connectionsDB.getConnection(connectionId)?.player
-  if (!player) { return }
+  if (!player) return;
 
   const room = roomsDB.addRoomUser(indexRoom, player)
-  if (!room) { return }
-  if (room.roomUsers.length < 2) { return }
+  if (!room) return;
+  if (room.roomUsers.length < 2) return;
 
   const game = gamesDB.createGame(room.roomId)
-
   gamesDB.setGamePlayers(game.gameId, room.roomUsers)
   roomsDB.closeRoomsWithPlayer(player)
 
-  connectionsDB.sendData(
-    roomsDB.getUpdateRoomData()
-  )
+  connectionsDB.sendData(roomsDB.getUpdateRoomData());
 
   gamesDB.getGameData(game.gameId)
     .forEach(({ type, data, connectionId }: any) => {
