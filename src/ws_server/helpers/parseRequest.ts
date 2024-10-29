bcc699adaf44c573c8903ce8d3cb1bb353323540
yourunb <bxyura@gmail.com>
2024-10-29 16:32:04 +0300

    feat: add sample env

--- src/ws_server/helpers/parseRequest.ts
+++ src/ws_server/helpers/parseRequest.ts
@@ -1,11 +1,11 @@
-import { ParsedRequestData, MsgType, Message } from '../types'
-
 export const parseRequestMsg = (message: string) => {
-  const msg = JSON.parse(message)
+  const msg = JSON.parse(message);
+  const {
+    type,
+    id,
+    data
+  } = msg as { type: any, id: number, data: string }
+  const parsedData: any = data ? JSON.parse(data) : '';
 
-  const { type, id, data } = msg as { type: MsgType, id: number, data: string }
-
-  const parsedData: ParsedRequestData = data ? JSON.parse(data) : ''
-
-  return { type, id, data: parsedData } as Message
+  return { type, id, data: parsedData } as any;
 }
--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -6,25 +6,25 @@ export const createServerWSS = () => {
 
   const wss = new WebSocketServer(
     { port: port, clientTracking: true },
-    () => { console.log(`${'WS SERVER: '} started on port: ${port}`) },
+    () => console.log(`${'WS SERVER: '} started on port: ${port}`),
   )
 
   wss.on('connection', (ws, request) => {
-    console.log(`${'WS:'} socket connection open. Origin: ${request.headers.origin}.`)
+    console.log(`${'WS:'} socket connection open. Origin: ${request.headers.origin}`)
 
     ws.on('message', (message: string) => {
       try {
         const msg = parseRequestMsg(message)
-        console.log(`<- : ${msg.type}: ${JSON.stringify(msg.data)}`)
+        console.log(`-> : ${msg.type}: ${JSON.stringify(msg.data)}`)
       } catch (err) {
-        console.log(`${'WS:'} ${'ERROR'} - internal Socket error. Origin: ${request.headers.origin}.`);
+        console.log(`${'WS:'} ${'ERROR'} - internal Socket error. Origin: ${request.headers.origin}`);
         ws.close()
       }
     })
   })
 
   wss.on('error', (error) => {
-    console.log(`${'WS SERVER:'} ${'ERROR'} - server error on port: ${port}. Error: ${error.message}.`);
+    console.log(`${'WS SERVER:'} ${'ERROR'} - server error on port: ${port}. Error: ${error.message}`);
   })
 
   wss.on('close', () => {
